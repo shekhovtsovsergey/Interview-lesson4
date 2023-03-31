35bd5fd2b58d1ce0a9f321a238b6bef81e38ead1
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2023-03-31 17:21:16 +0300

    Initial commit

---
+++ src/main/java/ru/gb/lesson4/InterviewLesson4Application.java
@@ -0,0 +1,13 @@
+package ru.gb.lesson4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewLesson4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewLesson4Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/lesson4/InterviewLesson4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.lesson4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewLesson4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
